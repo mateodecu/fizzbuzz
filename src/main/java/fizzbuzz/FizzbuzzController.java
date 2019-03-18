b90fb78d50da396e19cbf4c44783bfdddc5d7807
mateodecu <mateodecu@hotmail.com>
2019-03-18 10:44:15 +0100

    COMMENTS ADDED

--- src/main/java/fizzbuzz/FizzbuzzController.java
+++ src/main/java/fizzbuzz/FizzbuzzController.java
@@ -4,9 +4,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// CONTROLLER CLASS
+
+// CLASE CONTROLADOR
+
 @RestController
 public class FizzbuzzController {
 	
+	/*
+	MAPS AN HTTP REQUEST TO A JAVA CLASS METHOD USING SOME BASIC CRITERIA
+	
+	MAPEA UN LLAMADO HTTP A UNA CLASE DE JAVA USINDO CRITERIOS BASICOS
+	
+	
+	
+	THE @RequestParam TAG MAPS HTTP REQUEST VALUES TO THE METHOD VARIABLES
+	
+	LA ETIQUETA @RequestParam MAPEA VALORES DEL LLAMADO HTTP A LAS VARIABLES DEL METODO
+	*/
     @RequestMapping("fizzbuzz")
     public String valid(@RequestParam(value="numInicial", defaultValue="0") int numInicial,
 			 @RequestParam(value="numRango", defaultValue="10") int numRango) {
